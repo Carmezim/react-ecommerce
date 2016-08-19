e24467d2a50e21ff346bae265aeff76a9d502a23
Adriano <carmezim.filho@gmail.com>
2016-08-20 01:08:04 +0200

    missed some things on test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,18 @@
+import jsdom from 'jsdom';
 import React from 'react/addons';
 const { TestUtils } = React.addons;
 
-function renderShallow(component) {
-	const renderer = TestUtils.createRendere();
-	renderer.render(component);
-	return renderer.getRnderOutput();
+function setupFakeDOM() {
+	global.document = jsdom.jsdom("<!doctype html><html><body></body></html>);
+	global.window = document.parentWindow;
+	global.navigator = { userAgent: "node.js" };
+	require('react/lib/ExecutionEnvironment').canUseDOM = true
 }
 
-export { renderShallow };
+function renderShallow(component) {
+	const renderer = TestUtils.createRenderer();
+	renderer.render(component);
+	return renderer.getRenderOutput();
+}
+
+export { renderShallow, setupFakeDOM };
