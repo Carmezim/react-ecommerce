354372fef96fb8814818f6c88720456bc0b6257e
Adriano Carmezim <Carmezim.filho@gmail.com>
2016-09-16 04:42:08 +0200

    Updating server.js

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 import express from 'express';
 import fs from 'fs';
+import { Router } from 'react-router';
+import Location from 'react-router/lib/location';
+import routes from './src/routes';
+import ReactDOMServer from 'react-dom/server';
+import React from 'react';
+import Iso from 'iso';
+import alt from './src/alt';
 
 let app = express();
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname ++ '/public/index.html');
-});
-
 app.get('/orders.json', (req, res) => {
 	res.sendFile(__dirname + '/data/orders.json');
+});
+
+app.get('/sales_stats.json', (req, res) => {
+  res.sendFile(__dirname + '/data/sales_stats.json');
+});
+
+app.get('/orders', (req, res, next) => {
+  const orders = JSON.parse(fs.readFileSync(__dirname + '/data/orders.json'));
+
+  res.locals.data = {
+    OrdersStore: {
+      orders: orders,
+      selectedStatus: 'all',
+      amountFilter: null
+    }
+  };
+
+  next();
+});
+
+// Render UI
+app.use((req, res, next) => {
+  alt.bootstrap(JSON.stringify(res.locals.data || {}));
+  let iso = new Iso();
+
+  Router.run(routes, new Location(req.url), (error, props) => {
+    const content = ReactDOMServer.renderToString(<Router {...props} />);
+    iso.add(content, alt.flush());
+    res.render('index.ejs', { html: iso.render() });
+  });
 });
 
 const server = app.listen(8000, () => {
